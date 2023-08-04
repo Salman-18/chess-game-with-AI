59b70882c6b578f5448b6863995d075fdc5c7c59
Salman-18 <salmannizamani205@gmail.com>
2023-08-04 20:07:40 +0500

    made chess board

--- src/game.py
+++ src/game.py
@@ -1,11 +1,14 @@
 import pygame
 from const import *
 from board import Board
+from dragger import Dragger
 class Game:
     def __init__(self):
         self.board = Board()
+        self.dragger = Dragger()
+
     # show method
-    def show_bg(self, surface):
+    def show_bg(self, surface): 
         for  row in  range(ROWS):
             for col in range(COLS):
                 if (row + col) % 2 == 0:
@@ -21,9 +24,11 @@ class Game:
                 # pieces ?
                 if self.board.squares[row][col].has_piece():
                     piece = self.board.squares[row][col].piece
-                    
-                    img = pygame.image.load(piece.texture) 
-                    img_center = col * SQSIZE + SQSIZE // 2, row * SQSIZE + SQSIZE // 2
-                    piece.texture_rect = img.get_rect(center=img_center)
-                    surface.blit(img, piece.texture_rect)
+                    # all piece expect dragger piece
+                    if piece is not self.dragger.piece:
+                        piece.set_texture(size=80)
+                        img = pygame.image.load(piece.texture) 
+                        img_center = col * SQSIZE + SQSIZE // 2, row * SQSIZE + SQSIZE // 2
+                        piece.texture_rect = img.get_rect(center=img_center)
+                        surface.blit(img, piece.texture_rect)
 
