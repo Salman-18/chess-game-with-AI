0b0e3ddd0d9c1bd05e0fd548bad7ecb37a672432
Salman-18 <salmannizamani205@gmail.com>
2023-08-20 12:09:17 +0500

    Moving Pieces

--- src/const.py
+++ src/const.py
@@ -1,7 +1,9 @@
 #screen dimension 
+
 WIDTH = 600
 HEIGHT = 600
 # board dimsension
+
 ROWS = 8
 COLS = 8
 SQSIZE = WIDTH // COLS
--- src/dragger.py
+++ src/dragger.py
@@ -21,15 +21,19 @@ class Dragger:
         self.piece.texture_rect = img.get_rect(center=img_center)
         #blit
         surface.blit(img, self.piece.texture_rect)
+       
         # other method 
     def update_mouse(self, pos):
         self.mouseX, self.mouseY = pos #(xcor, ycor)
+    
     def save_initial(self, pos):  
         self.initial_row = pos[1] // SQSIZE
         self.initial_col = pos[0] // SQSIZE 
+    
     def drag_piece(self, piece):
         self.piece = piece
         self.dragging = True
+    
     def undrag_piece(self):
         self.piece = None
         self.dragging = False
