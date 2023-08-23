77e8817f3fac81346d9b20f529a168b651ac2963
Salman-18 <salmannizamani205@gmail.com>
2023-08-23 22:26:38 +0500

    Design Config

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 import pygame
 from sound import Sound
 from theme import Theme
+import os
 class Config:
     def __init__(self):
         self._theeme = []
@@ -8,10 +9,14 @@ class Config:
         self.idx = 0
         self.theme = self.themes[self.idx]
         # font
-        pass
+        self.move_sound = Sound(
+            os.path.join("assets/sound/move.wav"))
+        self.caputre_sound = Sound(
+            os.path.join("assets/sound/capture.wav"))
     
     def change_theme(self):
-        pass
+        self.ixd += 1
+        
 
     def _add_themes(self):
         pass 
